de220a4793a4e081ea859241c66a36d234e49500
Sam Fortuna <saf1119@gmail.com>
2018-07-30 18:15:07 -0500

    final updates

--- mergesort.js
+++ mergesort.js
@@ -7,27 +7,10 @@ function split(array) {
 	return [firstHalf, secondHalf]
 }
 
-function splitRecursive (wholeArray) {
-
-	if ( wholeArray.length > 1){
-		let firstSplit = split(wholeArray)
-		splitRecursive(firstSplit[0])
-		splitRecursive(firstSplit[1])	
-	}	
-
-	if(wholeArray.length === 1){
-		splitArray.push(wholeArray)
-	}
-
-	return splitArray;
-}
-
 function merge (arrayOne, arrayTwo) {
 	let mergeArray = [];
 	let headArrayOne = 0;
 	let headArrayTwo = 0;
-	console.log(arrayOne)
-	console.log(arrayTwo)
 	while ((headArrayTwo < arrayTwo.length) || (headArrayOne < arrayOne.length)){
 		if (arrayOne[headArrayOne] < arrayTwo[headArrayTwo] || (arrayTwo[headArrayTwo] === undefined)){
 			mergeArray.push(arrayOne[headArrayOne])
@@ -40,33 +23,12 @@ function merge (arrayOne, arrayTwo) {
 	return mergeArray;
 }
 
-function mergeRecursive(nestedArray){
+function mergeSort (wholeArray) {
 
-	let index = Math.floor(nestedArray.length / 2);
-	let firstHalfFinal, secondHalfFinal;
-	let firstHalf = nestedArray.slice(0,index);
-	let secondHalf = nestedArray.slice(index);
-
-	if (firstHalf.length === 2){
-	 	firstHalfFinal = merge(firstHalf[0], firstHalf[1]);
-	} else if (firstHalf.length > 2) {
-		firstHalfFinal = mergeRecursive(firstHalf)
-	}else{
-		firstHalfFinal = firstHalf[0];
+	if ( wholeArray.length > 1){
+		let firstSplit = split(wholeArray)
+		return merge(mergeSort(firstSplit[0]), mergeSort(firstSplit[1]));
+	} else {
+		return wholeArray;
 	}
-	if (secondHalf.length === 2){
-		secondHalfFinal = merge(secondHalf[0],secondHalf[1])
-	}else if (secondHalf.length > 2){
-		secondHalfFinal = mergeRecursive(secondHalf)
-	} else{
-		secondHalfFinal = secondHalf[0];
-	}
-	return merge(firstHalfFinal, secondHalfFinal)
 }
-
-function mergeSort(array) {
-	let arrayToMerge = splitRecursive(array);
-	return mergeRecursive(arrayToMerge);
-	
-}
-//console.log(mergeSort([1,2,3,4,5,6]))
